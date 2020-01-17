65e38048460485aab0d383b2223b09d524f90f8d
takayukioda <takayuki@odashi.me>
2020-01-17 16:37:54 +0900

    simplify codes

--- how-it-works/promise-and-try/index.ts
+++ how-it-works/promise-and-try/index.ts
@@ -1,72 +1,66 @@
-export const TryController = async (n: number): Promise<number | undefined> => {
-  try {
-    return await AwaitService(n)
-  } catch (err) {
-    throw err
+export const Controller = async (n: number): Promise<number | undefined> => {
+  console.log('--> Controller')
+  const core = await TheService(n);
+
+  AsyncService(5).catch(() => console.log('Got an error; continuing the process'));
+  await TheService(4);
+  
+  return core
+}
+
+export const TheService = async (n: number): Promise<number> => {
+  console.log('---> TheService')
+  const ok = await AsyncValidate(n)
+  if (!ok) {
+    throw new Error(`Validation not ok: ${n}`)
   }
-}
 
-export const NoTryController = async (n: number): Promise<number | undefined> => {
-  return await AwaitService(n)
-}
-
-
-export const AwaitService = async (n: number): Promise<number> => {
-  await Validate(n)
   return Save(n % 5)
 }
 
-export const NoAwaitService = async (n: number): Promise<number> => {
-  Validate(n)
-  return Save(n % 5)
+export const AsyncService = async (n: number): Promise<number> => {
+  console.log('---> AsyncService')
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (SyncValidate(n)) {
+        console.log('AsyncService resolved');
+        return resolve(n);
+      }
+        console.log('AsyncService rejected');
+      return reject(n);
+    }, n * 100)
+  });
 }
 
+export const SyncValidate = (n: number): boolean => {
+  console.log('----> SyncValidate');
+  return n % 5 !== 0;
+}
 
-export const Validate = async (n: number): Promise<void> => {
-  if (n % 5 === 0) {
-    throw new Error('Oooops, found an invalid value!')
-  }
+export const AsyncValidate = async (n: number): Promise<boolean> => {
+  console.log('----> AsyncValidate');
+  return n % 5 !== 0;
 }
 
 export const Save = async (n: number): Promise<number> => {
+  console.log('----> Save')
   if (n % 3 === 0) {
-    throw new Error('Oooops, cannot save this value!')
+    throw new Error(`Oooops, cannot save this value! ${n}`)
   }
+
   return n
 }
 
 (async (go: boolean) => {
   if (!go) return;
-  try {
-    console.log('== Await at last ==');
-    const tc1 = TryController(1);
-    const tc2 = TryController(2);
-    const tc3 = TryController(3); // cannot save value
-    const tc4 = TryController(4);
-    const tc5 = TryController(5); // invalid value
+  console.log('=> Starting behaviour check');
 
-    await tc1;
-    await tc2;
-    await tc3;
-    await tc4;
-    await tc5;
-  } catch (err) {
-    console.log('Found an error');
-    console.error(err.message);
-  }
-})(false);
-
-(async (go: boolean) => {
-  if (!go) return;
   try {
-    console.log('== Await for each ==');
-    await TryController(1);
-    await TryController(2);
-    await TryController(3); // cannot save value
-    await TryController(4);
-    await TryController(5); // invalid value
+    await Controller(7);
   } catch (err) {
-    console.log('Found an error');
-    console.error(err.message);
+    console.error('----- I catched it!! -----');
+    console.error(err)
+    console.error('----- ----- -- ----- -----')
   }
 })(true);
